4807a97212dc61129020175d9a0c089832623788
blackfox <huangy_tao@126.com>
2017-05-11 11:32:33 +0800

    将项目导入到EC

---
+++ src/com/andy/util/DBUtil.java
@@ -0,0 +1,69 @@
+package com.andy.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class DBUtil {
+	private static Connection conn=null;
+	private static Statement stm=null;
+	
+	public static Connection getConnection(){
+//		String url="jdbc:oracle:thin:@127.0.0.1:1521:orcl";
+		String url="jdbc:mysql://192.168.60.94:3306/onlineexam?user=root&password=root&useUnicode=true&characterEncoding=UTF-8";
+		String userName="root";
+		String password="root";
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			conn=DriverManager.getConnection(url, userName, password);
+			stm=conn.createStatement();
+		} catch (Exception e1) {
+			e1.printStackTrace();
+		}
+		return conn;
+	}
+	public static ResultSet executeQuery(String sql){
+		ResultSet rs=null;
+		try {
+			rs=stm.executeQuery(sql);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return rs;
+	}
+	public static int executeUpdate(String sql){
+		int count=0;
+		try {
+			count=stm.executeUpdate(sql);
+		} catch (SQLException e) {
+			count=0;
+			e.printStackTrace();
+		}
+		return count;
+		
+	}
+	public static void commit(){
+		try {
+			conn.commit();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	public static void close(){
+		try {
+			if(stm!=null){
+				stm.close();
+				stm=null;
+			}
+			if(conn!=null){
+				conn.close();
+				conn=null;
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
